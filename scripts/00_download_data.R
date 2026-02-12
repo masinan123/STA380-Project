8629c8e4ddf7f668169deb230c857e296a616373
Sinan Ma <156612938+masinan123@users.noreply.github.com>
2026-02-12 00:34:15 -0500

    Update 00_download_data.R

--- scripts/00_download_data.R
+++ scripts/00_download_data.R
@@ -12,8 +12,6 @@ dir.create("submissions/Data/Raw", showWarnings = FALSE)
 file_path <- "submissions/Data/Raw/WPP2024_Complete.xlsx"
 
 #### Data download ####
-# note: replace `download_url` with the DIRECT download link for:
-# "Complete (estimates and all projection scenarios) (XLSX)"
 download_url <- "https://population.un.org/wpp/assets/Excel%20Files/1_Indicator%20(Standard)/EXCEL_FILES/1_General/WPP2024_GEN_F01_DEMOGRAPHIC_INDICATORS_FULL.xlsx"
 
 resp <- GET(download_url, write_disk(file_path, overwrite = TRUE))

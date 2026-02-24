c5def1607c3347d12285ff2350f45dbedabe7a10
lizhe114 <zhengjia.li@mail.utoronto.ca>
2026-02-24 12:59:56 -0500

    added a test-plot_perm_dist file

---
+++ submissions/BasicImplementation/plot_perm_dist.R
@@ -0,0 +1,58 @@
+#' Plot the Permutation Distribution with Observed Statistic
+#'
+#' Draws a histogram of the permutation test statistic and shows the observed 
+#' test statistic on the histogram as a vertical line. 
+#'
+#' @param perm_result A list containing: 
+#' \describe{
+#'    \item{perm}{Numeric vector of permutation statistic. }
+#'    \item{obs}{Observed test statistic. }
+#' }
+#' @param bin An Integer specifying the number of histogram bins. Defaults to 40. 
+#' @return Invisibly returns a list with:
+#' \describe{
+#'  \item{hist}{Histogram object returned by function hist().}
+#'  \item{obs}{Observed test statistic. }
+#' }
+#' 
+#' @examples
+#' perm_res <- list(perm = abs(rnorm(1000)), obs = 2.5)
+#' plot_perm_dist(perm_res)
+#' 
+#' @export
+plot_perm_dist <- function(perm_result, bins = 40) {
+  
+  perm <- perm_result$perm
+  obs <- perm_result$obs
+  
+  max_perm <- max(perm)
+  extension <- 0.2 * (max_perm - min(perm))
+  x_right <- max(max_perm + extension, obs)
+  
+  h <- hist(perm, 
+            breaks = bins, 
+            col = "lightblue", 
+            border = "white", 
+            main = "Permutation Distribution", 
+            xlab = "Absolute Difference in Means",
+            xlim = c(min(perm), x_right) )
+  
+  abline(v = obs, col = "red", lwd = 3)
+  
+  perm_extreme <- perm[perm >= obs]
+  if (length(perm_extreme) > 0) {
+    hist(perm_extreme,
+         breaks = bins,
+         col = rgb(0, 1, 0, 0.4),
+         add = TRUE)
+  }
+  # return the hist object by invisible()
+  invisible(list(
+    hist = h,
+    obs  = obs,
+    perm = perm
+  ))
+}
+
+
+# AI usage: the usage of invisible() and rgb()

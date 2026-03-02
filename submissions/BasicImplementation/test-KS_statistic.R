91e6d93f4213d17d94e0980d36e39f4b8a9ba603
lizhe114 <zhengjia.li@mail.utoronto.ca>
2026-03-02 18:15:24 -0500

    added more tests for ks statistic and removed mean and median in perm_functions

--- submissions/BasicImplementation/test-KS_statistic.R
+++ submissions/BasicImplementation/test-KS_statistic.R
@@ -12,6 +12,33 @@ test_that("test perm_stat_ks calculates correctly", {
 })
 
 
+test_that("perm_stat_ks returns 1 for completely separated groups", {
+  outcome <- c(1, 2, 3, 10, 11, 12)
+  group <- factor(rep(c("More developed", "Less developed"), each = 3))
+  
+  ks <- perm_stat_ks(outcome, group)
+  testthat::expect_equal(ks, 1)
+})
+
+
+test_that("perm_stat_ks errors if group does not have exactly two groups", {
+  outcome <- c(1, 2, 3)
+  group <- c("A", "A", "A")
+  
+  expect_error(perm_stat_ks(outcome, group), "group must have exactly two levels. ")
+})
+
+
+test_that("test perm_stat_ks on unequal group size", {
+  outcome = c(1, 3, 5, 2, 4)
+  group = factor(c("More developed", "More developed", "More developed", 
+                   "Less developed", "Less developed"))
+  res <- perm_stat_ks(outcome, group)
+  
+  expect_equal(res, 1/3)
+})
+
+
 test_that("test perm_test_two_group_ks returns correctly", {
   d2 <- data.frame(
     outcome = c(1, 3, 5, 2, 4, 6), 
@@ -23,15 +50,5 @@ test_that("test perm_test_two_group_ks returns correctly", {
   
   expect_equal(obs, pval$observed)
   expect_equal(pval$p_value, 1)
-})
-
-
-test_that("test perm_stat_ks on unequal group size", {
-  outcome = c(1, 3, 5, 2, 4)
-  group = factor(c("More developed", "More developed", "More developed", 
-                   "Less developed", "Less developed"))
-  res <- perm_stat_ks(outcome, group)
-  
-  expect_equal(res, 1/3)
 })
 
